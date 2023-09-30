7b1888430fa08e13a865f3f0d651c82e916628ff
kytecat <87901430+kytecat@users.noreply.github.com>
2023-09-30 18:56:42 +0200

    Add Darkmode Support

--- Home.Ui.BlazorSrv/MainLayout.razor.cs
+++ Home.Ui.BlazorSrv/MainLayout.razor.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Smoehring.Home.Ui.BlazorSrv
 {
     public partial class MainLayout : LayoutComponentBase
     {
         private bool _drawerOpen = true;
+        private MudThemeProvider _mudThemeProvider;
+        private bool _isDarkmode;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _isDarkmode = await _mudThemeProvider.GetSystemPreference(); 
+                StateHasChanged();
+            }
+        }
 
         private void DrawerToggle()
         {
